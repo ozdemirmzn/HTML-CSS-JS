08af112a1d5955f01eee8bb090403d54c5b9a19e
ozdemirmzn <ozdemirmzn@gmail.com>
2020-09-18 23:58:18 -0500

    final work

--- script.js
+++ script.js
@@ -31,15 +31,47 @@ window.addEventListener("load", function() {
               });
             });
 
-    // form validation 
+    
     let form = document.querySelector("form");
     form.addEventListener("submit", function(event) {
+        event.preventDefault();
        let pilot = document.querySelector("input[name=pilotName]");
        let copilot = document.querySelector("input[name=copilotName]");
        let fuel = document.querySelector("input[name=fuelLevel]");
        let cargo = document.querySelector("input[name=cargoMass]");
 
-    
+       let launchStatus = document.getElementById("launchStatus");
+       let faultyItems = document.getElementById("faultyItems");
+       let pilotStatus = document.getElementById("pilotStatus");
+       let copilotStatus = document.getElementById("copilotStatus");
+       let fuelStatus = document.getElementById("fuelStatus");
+       let cargoStatus = document.getElementById("cargoStatus");
+
+       // shuttle status
+
+       pilotStatus.innerHTML = `Pilot ${pilot.value} is ready for launch.`;
+       copilotStatus.innerHTML= `Co-pilot ${copilot.value} is ready for launch`;
+       if(fuel.value>=10000){
+            fuelStatus.innerHTML= `Fuel level is good enough for launch`;}
+       else{fuelStatus.innerHTML= `Fuel level is too low for launch`;}
+       if(cargo.value<10000){
+           cargoStatus.innerHTML = `Cargo mass is low enough for launch`;}
+       else{
+            cargoStatus.innerHTML = `Cargo mass is too high for launch`;}
+
+        if(fuel.value>=10000 && cargo.value<10000){
+            launchStatus.innerHTML = `Shuttle is ready for launch`;
+            launchStatus.style.color = "green";
+            faultyItems.style.visibility = "visible";
+
+        }else{
+            launchStatus.innerHTML = `Shuttle is not ready for launch`;
+            launchStatus.style.color = "red";
+            faultyItems.style.visibility = "visible";
+        }
+
+
+            // form validation 
 
               if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
                   alert("All fields are required!");
@@ -49,6 +81,10 @@ window.addEventListener("load", function() {
                     alert("Make sure to enter valid information for each field!");
                     event.preventDefault();
               }
-              
+    
+         
+
+
+
     });
  });
